97c52e5b958795242f97a15da582e4efabf40ff9
Chris Priest <cp368202@ohiou.edu>
2016-03-05 23:40:44 -0800

    moved api docs to seperate page

--- multiexplorer/multiexplorer/urls.py
+++ multiexplorer/multiexplorer/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 from views import home, perform_lookup
 
 urlpatterns = [
@@ -9,5 +11,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name="home"),
+    url(r'api$', TemplateView.as_view(template_name='api_docs.html')),
     url(r'api/(?P<currency>\w+)/(?P<service_mode>\w+)/(?P<service_id>\w+)', perform_lookup, name="perform_lookup"),
 ]
