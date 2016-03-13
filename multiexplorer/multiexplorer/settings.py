225fb918429269ae1195386fb1796e47b1f0f9b5
Chris Priest <cp368202@ohiou.edu>
2016-03-13 14:37:03 -0700

    removed database from settings file

--- multiexplorer/multiexplorer/settings.py
+++ multiexplorer/multiexplorer/settings.py
@@ -72,18 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'multiexplorer.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
