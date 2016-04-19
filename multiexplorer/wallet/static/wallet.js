6454d936a8cb176a3865016aed556fd2282fb005
Chris Priest <cp368202@ohiou.edu>
2016-04-19 14:03:27 -0700

    refactored client side address derivation code

--- multiexplorer/wallet/static/wallet.js
+++ multiexplorer/wallet/static/wallet.js
@@ -1,4 +1,4 @@
-bip44_coin_types = {};
+var bip44_coin_types = {};
 $.each(crypto_data, function(i, data) {
 
     var network = {};
@@ -13,7 +13,7 @@ $.each(crypto_data, function(i, data) {
     }
 });
 
-function get_deposit_address(crypto, change, index) {
+function get_crypto_root(crypto) {
     var bip44_coin_type = undefined;
     var address_byte = undefined;
     var bip44 = bip44_coin_types[crypto];
@@ -23,10 +23,32 @@ function get_deposit_address(crypto, change, index) {
     }
 
     var network = bitcore.Networks.get(crypto);
-    var xpriv = hd_master_seed.derive(44, true) //purpose
 
-    //           coin type       account             change             index
-    xpriv = xpriv.derive(bip44).derive(1, true).derive(change ? 1 : 0).derive(index);
+    //                         purpose       coin type
+    return hd_master_seed.derive(44, true).derive(bip44);
+}
+
+function derive_addresses(xpriv, crypto, change, index) {
+    //           account             change             index
+    xpriv = xpriv.derive(0, true).derive(change ? 1 : 0).derive(index);
     var wif = bitcore.PrivateKey(xpriv.privateKey.toString(), crypto).toWIF();
-    return [wif, xpriv.privateKey.toAddress(crypto).toString()]
+    return [wif, xpriv.privateKey.toAddress(crypto).toString()];
+}
+
+function get_deposit_keypair(crypto, index) {
+    return derive_addresses(get_crypto_root(crypto), crypto, false, index);
+}
+
+function get_change_keypair(crypto, index) {
+    return derive_addresses(get_crypto_root(crypto), crypto, true, index);
+}
+
+function get_balance_for_crypto(crypto) {
+    var xpriv = get_crypto_root(crypto);
+
+    var args = "?xpriv=" + xpriv.toString() + "&crypto=" + crypto;
+    $.ajax({
+        'url': BALANCE_XPRIV_URL + args,
+        'type': 'get',
+    })
 }
