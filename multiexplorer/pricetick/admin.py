d32535427a6fb4c6ed7549639507e5609bceb9f9
Chris Priest <nbvfour@gmail.com>
2017-02-26 00:40:57 -0800

    default ordering in pricetick admin

--- multiexplorer/pricetick/admin.py
+++ multiexplorer/pricetick/admin.py
@@ -3,5 +3,6 @@ from models import PriceTick
 
 class PriceTickAdmin(admin.ModelAdmin):
     list_display = ['date', 'base_fiat', 'currency', 'exchange', 'price']
+    ordering = ('-date', )
 
 admin.site.register(PriceTick, PriceTickAdmin)
