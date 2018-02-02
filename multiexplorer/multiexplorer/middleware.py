319c9316cba4e67445d80130a4b6f42ad96e7f67
Chris Priest <cp368202@ohiou.edu>
2018-02-01 19:03:27 -0700

    made more verbose error message on 429

--- multiexplorer/multiexplorer/middleware.py
+++ multiexplorer/multiexplorer/middleware.py
@@ -1,7 +1,12 @@
 from multiexplorer.models import IPTracker
 from django.http import HttpResponse
+from django.conf import settings
 
 class IPLimiterMiddleware(object):
+    interval = "%s %s" % (
+        settings.IP_FILTER_INTERVAL.values()[0],
+        settings.IP_FILTER_INTERVAL.keys()[0],
+    )
 
     def __init__(self, get_response):
         self.get_response = get_response
@@ -14,7 +19,9 @@ class IPLimiterMiddleware(object):
             return response
 
         return HttpResponse(
-            '{"error": "Too many requests"}',
+            '{"error": "Too many requests. The limit is %s requests per %s"}' % (
+                settings.IP_FILTER_HITS, self.interval
+            ),
             content_type="application/json",
             status=429
         )
