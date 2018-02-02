fa8d9644890570e060b65cf3b91a097acf146198
Chris Priest <cp368202@ohiou.edu>
2018-02-01 18:23:13 -0700

    added middleware

---
+++ multiexplorer/multiexplorer/middleware.py
@@ -0,0 +1,19 @@
+from multiexplorer.models import IPTracker
+from django.http import HttpResponse
+
+class IPLimiterMiddleware(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip = request.META['REMOTE_ADDR']
+        if IPTracker.allow(ip):
+            response = self.get_response(request)
+            return response
+
+        return HttpResponse(
+            '{"error": "Too many requests"}',
+            content_type="application/json",
+            status=429
+        )
